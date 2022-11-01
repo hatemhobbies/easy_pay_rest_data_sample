48a4b67571c299aa17c8574f271881b95d3ff2eb
hatemhobbies <102426885+hatemhobbies@users.noreply.github.com>
2022-11-01 01:50:59 -0500

    Update buildAndStartContainers.sh

--- buildAndStartContainers.sh
+++ buildAndStartContainers.sh
@@ -12,8 +12,18 @@ docker network create easypaynet
 rm -rf /var/lib/postgresql/data/pgdata
 rm -rf /tmp/data
 
-docker run --name basic-postgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp/data:/var/lib/postgresql/data -p 5432:5432 -d postgres:14.1-alpine
-POSTGRES_CONTAINER_IP=`docker inspect -f \ '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' epmysql`
+docker run --name empostgres -postgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp/data:/var/lib/postgresql/data -p 5432:5432 -d postgres:14.1-alpine
+POSTGRESSQL_CONTAINER_IP=`docker inspect -f \ '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' empostgres `
 sleep 10
-docker run --name walletservice --network=easypaynet  -p 8080:8080 -e POSTGRES_HOST="${POSTGRES_CONTAINER_IP## }"  hatemhobbies/wallet:2.0
+docker run --name walletservice --network=easypaynet  -p 8080:8080 -e POSTGRES_HOST="${POSTGRESSQL_CONTAINER_IP## }"  hatemhobbies/wallet:2.0
 
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                                                                                                                                                                                                                             
+~                                                        
