9d03ccbcb908149d126d82caf503199b6293aaa6
hatemhobbies <102426885+hatemhobbies@users.noreply.github.com>
2022-11-01 02:02:53 -0500

    Update buildAndStartContainers.sh

--- buildAndStartContainers.sh
+++ buildAndStartContainers.sh
@@ -12,10 +12,11 @@ docker network create easypaynet
 rm -rf /var/lib/postgresql/data/pgdata
 rm -rf /tmp/data
 
-docker run --name empostgres -postgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp/data:/var/lib/postgresql/data -p 5432:5432 -d postgres:14.1-alpine
+docker run --name empostgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp/data:/var/lib/postgresql/data -p 5432:5432 -d postgres:14.1-alpine
 POSTGRESSQL_CONTAINER_IP=`docker inspect -f \ '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' empostgres `
 sleep 10
 docker run --name walletservice --network=easypaynet  -p 8080:8080 -e POSTGRES_HOST="${POSTGRESSQL_CONTAINER_IP## }"  hatemhobbies/wallet:2.0
+
 
 ~                                                                                                                                                                                                                                                             
 ~                                                                                                                                                                                                                                                             
